3091b5972a3441fa4e06b2246422d20a4f1ac1b8
xiaobai <948206388@qq.com>
2017-03-25 16:41:35 +0800

    add img.js and change img

---
+++ src/img.js
@@ -0,0 +1,7 @@
+const imgUrl = {
+	indexGroup: '../static/img-peo/upload_ie4tqmjwmvrdcyrrgyzdambqgqyde_564x211.jpg',
+	indexList: ['../static/img-peo/upload_ie3wmmtcge2tcyrrgyzdambqgayde_181x162.jpg', '../static/img-peo/upload_ifqtcnbtgi2tcyrrgyzdambqgyyde_181x162.jpg', '../static/img-peo/upload_ifrdkntcgi2tcyrrgyzdambqmeyde_181x162.jpg', '../static/img-peo/upload_ifqtsojtgm2tcyrrgyzdambqgyyde_181x162.jpg', '../static/img-peo/upload_ie4giylcgm2tcyrrgyzdambqgiyde_181x162.jpg', '../static/img-peo/upload_ie3tayztgq2tcyrrgyzdambqgayde_181x162.jpg',]
+}
+
+
+export default imgUrl
